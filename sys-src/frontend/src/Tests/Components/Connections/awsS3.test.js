27ee69a0bc830e8860d41e5227a6cdc02e73a06d
André <62932745+AndreKest@users.noreply.github.com>
2023-06-30 08:10:20 +0200

    Update awsS3.test.js

--- sys-src/frontend/src/Tests/Components/Connections/awsS3.test.js
+++ sys-src/frontend/src/Tests/Components/Connections/awsS3.test.js
@@ -20,9 +20,9 @@ describe("s3Manager - connection", () => {
         expect(s3Manager.region).not.toBe("");
 
         // check if credentials are set to env
-        expect(s3Manager.accessKeyId).toBe("AKIAZUXPDVDVIX7TZC6V");
-        expect(s3Manager.secretAccessKey).toBe("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");
-        expect(s3Manager.region).toBe("eu-central-1");
+        expect(s3Manager.accessKeyId).toBe("");
+        expect(s3Manager.secretAccessKey).toBe("");
+        expect(s3Manager.region).toBe("");
     });
 
     // skip test because no access to S3 -> will be changed next week
